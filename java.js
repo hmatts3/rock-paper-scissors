0aa8797e2fbacd4d442e84f9540fac495f46b835
Hans <hans.matta@gmail.com>
2023-02-02 16:44:36 -0500

    debug*100000, loop

--- java.js
+++ java.js
@@ -9,69 +9,72 @@ function getComputerChoice () {
 }
 
 // Allow user to input. (define variable)
-// If both selections are equal, then 'Tie! Try again'
-// define Rock > Paper, Paper > Scissors, Scissors > Paper
-// if computerSelection > playerSelection, return "You Lose!" & compare selections
 // edge case: convert player selection to first upper case, rest lower case
 
 let playerSelection = 'rock';
 function convertPlayerSelectionCase (playerSelection) {
     return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 }
-
 playerSelection = convertPlayerSelectionCase(playerSelection)
 
 
-function playRound(playerSelection, computerSelection) {
-    let Rock = 'Rock';
-    let Paper = 'Paper';
-    let Scissors = 'Scissors';
-    Rock > Scissors;
-    Paper > Rock;
-    Scissors > Paper;
-
-    if (playerSelection > computerSelection) {
-        return `${playerSelection} \n${computerSelection} \nYou Win! ${playerSelection} beats ${computerSelection}.`
-    }
-
-    else if (computerSelection > playerSelection) {
-        return `${playerSelection} \n${computerSelection} \nYou Lose! ${computerSelection} beats ${playerSelection}.`
-    }
-
-    else {
-        return `${playerSelection} \n${computerSelection} \nTie! Try again!`
-    }
-
-}
+// If both selections are equal, then 'Tie! Try again'
+// define Rock > Paper, Paper > Scissors, Scissors > Paper
+// if computerSelection > playerSelection, return "You Lose!" & compare selections
 
 const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
-
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `${playerSelection} \n${computerSelection} \nTie! Try again!`
+    }
+    else if ((playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper') || (playerSelection === 'Rock' && computerSelection === 'Scissors')) {
+        return `${playerSelection} \n${computerSelection} \nYou Win! ${playerSelection} beats ${computerSelection}.`
+    }
+    else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || (playerSelection === 'Paper' && computerSelection === 'Scissors') || (playerSelection === 'Scissors' && computerSelection === 'Rock')){
+        return `${playerSelection} \n${computerSelection} \nYou Lose! ${computerSelection} beats ${playerSelection}.`
+    }
+    else {
+        return `You entered an incorrect value.`
+    }
+}
 
 
 // play 5 rounds (up to 5 playerSelection s 5 computer Selection s)
-// Keep score after each round. 
-// Declares a winner at the end (first to 3 wins)
+// return score after each round. (In addition to previous message)
+// User inputs new value each round
+// After 5 rounds, return the winner and final score
+// I want it to ask me for a selection, run the function playRound, display the result of playRound, plus the score of this round, and then at the end say game over
 
-for (let i = 0; i <5; i++) {
-    function game() {
-        playerSelection = prompt('Pick one','')
-        playRound(playerSelection, computerSelection);
-        let playerWins= 0;
-        let cpuWins = 0;
-    if (playerSelection > computerSelection) {
-        playerWins++ ;
-        return `You Win. The score is ${playerWins}-${cpuWins}`;
+
+
+// What's happening? If I run game(), then if I win, I get 1 + playerwin. Playerwin = 0. 
+
+function game () {
+    
+    console.log(playRound(playerSelection, computerSelection));
+    if  ((playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper') || (playerSelection === 'Rock' && computerSelection === 'Scissors')) {
+        return `The score is ${++playerWin}-${cpuWin}`
     }
-    else if (computerSelection > playerSelection) {
-        cpuWins++;
-        return `You Lose. The score is ${playerWins}-${cpuWins}`;
+    else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || (playerSelection === 'Paper' && computerSelection === 'Scissors') || (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+        return 
     }
     else {
-        return `Tie. The score is ${playerWins}-${cpuWins}`
+        return `The score is ${playerWin}-${cpuWin}`
     }
+}
+
+
+for (let i=0; i<5; i++) {
+    let playerWin = 0;
+    let cpuWin = 0;
+    playerSelection = prompt();
+    playRound(playerSelection, computerSelection);
+    if  ((i < 5) && (playerSelection === 'Rock' && computerSelection === 'Paper') || (playerSelection === 'Paper' && computerSelection === 'Scissors') || (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+        console.log(playRound(playerSelection,computerSelection) + `The score is ${playerWin}-${++cpuWin}`)
     }
+    else if  ((i < 5) && (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper') || (playerSelection === 'Rock' && computerSelection === 'Scissors')) {
+        console.log(playRound(playerSelection,computerSelection) + `The score is ${++playerWin}-${cpuWin}`)
     }
 
-game()
+    }
---
+++ java2.js
@@ -0,0 +1,73 @@
+// generate a random function for CPU to pick
+
+const RockPaperScissors = ['Rock', 'Paper', 'Scissors']
+
+function getComputerChoice () {
+    const random = Math.floor(Math.random() * RockPaperScissors.length);
+    return (RockPaperScissors[random]);
+}
+
+let computerSelection = getComputerChoice();
+
+// prompt player userInput; convert to first upper case, rest lower case
+
+let userInput;
+
+function convertCase (userInput) {
+    userInput = prompt();
+    if (userInput === null) {
+        return;
+    }
+    else {
+    return userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+    }
+}
+
+// After conversion of player choice, & random CPU choice, if (Paper > Rock)... etc., restate both choices and describe Winner. 
+
+let cpuWins = 0;
+let playerWins = 0;
+
+function playRound(userInput, computerSelection) {
+    if (userInput === computerSelection) {
+        return `You chose ${userInput} \nCPU chose ${computerSelection} \nTie! Try again! \nThe score is ${playerWins}-${cpuWins}`
+    }
+    else if ((userInput === 'Paper' && computerSelection === 'Rock') || (userInput === 'Scissors' && computerSelection === 'Paper') || (userInput === 'Rock' && computerSelection === 'Scissors')) {
+        return `You chose ${userInput} \nCPU chose ${computerSelection} \nYou Win! ${userInput} beats ${computerSelection}. \nThe score is ${++playerWins}-${cpuWins}`
+    }
+    else if ((userInput === 'Rock' && computerSelection === 'Paper') || (userInput === 'Paper' && computerSelection === 'Scissors') || (userInput === 'Scissors' && computerSelection === 'Rock')){
+        return `You chose ${userInput} \nCPU chose ${computerSelection} \nYou Lose! ${computerSelection} beats ${userInput}. \nThe score is ${playerWins}-${++cpuWins}`
+    }
+    else if (userInput===null) {
+        return;
+    }
+    else {
+        return `You entered an incorrect value. \nThe score is ${playerWins}-${cpuWins}`
+    }
+}
+
+// added a counter for the score.
+// Loop the game so that it repeats 5 times, and at the end, declares a winner. 
+
+
+
+for (let i=0; ;i++) {
+  userInput = convertCase(userInput);
+  computerSelection = getComputerChoice();
+  console.log(playRound(userInput, computerSelection));
+  if (playerWins===3) {
+    alert(`You win! The final score is ${playerWins}-${cpuWins}`);
+    console.log('Game over. You Win!')
+    break;
+  } 
+  else if (userInput===null) {
+    break;
+  }
+  else if (cpuWins===3) {
+    alert(`Game over. You Lose. The final score is ${playerWins}-${cpuWins}`);
+    console.log('Game over. You lose.')
+    break;
+  } 
+}
+
+// Instead, make it first to 3 (Best of 5. Declare a winner)
